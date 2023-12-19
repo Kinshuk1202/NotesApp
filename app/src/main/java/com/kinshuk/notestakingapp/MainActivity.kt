bef2a65f38f661f3619c72d101f45e33d4ac726a
Kinshuk1202 <khare.kinshuk03@gmail.com>
2023-12-19 15:12:14 +0530

    App Lock option Added

--- app/src/main/java/com/kinshuk/notestakingapp/MainActivity.kt
+++ app/src/main/java/com/kinshuk/notestakingapp/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.kinshuk.notestakingapp
 
+import android.app.KeyguardManager
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
@@ -15,13 +20,60 @@ import com.kinshuk.notestakingapp.viewmodel.NoteViewModelFactory
 class MainActivity : AppCompatActivity() {
     lateinit var noteViewModel:NoteViewModel
     lateinit var binding:ActivityMainBinding
+    private val CODE_AUTHENTICATION_VERIFICATION = 241
+    private lateinit var  sharedPref: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setUpViewModel()
-
         supportActionBar?.setBackgroundDrawable(getDrawable(R.color.actionBarColor))
+
+        sharedPref = getPreferences(Context.MODE_PRIVATE) ?: return
+        with(sharedPref.edit()){
+            if(!sharedPref.contains(getString(R.string.sharedPrefKey)))
+            {
+                putBoolean(getString(R.string.sharedPrefKey),false)
+                apply()
+            }
+        }
+        val enabled = sharedPref.getBoolean(getString(R.string.sharedPrefKey),false)
+        if(enabled) {
+            binding.et.visibility = View.VISIBLE
+            LockScreen()
+        }
+        else
+            binding.et.visibility = View.GONE
+    }
+
+    private fun LockScreen() {
+        val km = getSystemService(KEYGUARD_SERVICE) as KeyguardManager
+        if (km.isKeyguardSecure) {
+            val i =
+                km.createConfirmDeviceCredentialIntent("Authentication required", "Use your password")
+            startActivityForResult(i, CODE_AUTHENTICATION_VERIFICATION)
+        }
+        else {
+            Toast.makeText(
+                this,
+                "Need to setup Phone Lock Screen Authentication to use App Lock",
+                Toast.LENGTH_LONG
+            ).show()
+            sharedPref.edit().putBoolean(getString(R.string.sharedPrefKey),false).apply()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == CODE_AUTHENTICATION_VERIFICATION) {
+            if (resultCode == RESULT_OK) {
+                binding.et.visibility = View.GONE
+                Toast.makeText(this, "Welcome!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Failure: Unable to verify user's identity", Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
     }
 
     private fun setUpViewModel() {
--- app/src/main/java/com/kinshuk/notestakingapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/kinshuk/notestakingapp/fragments/HomeFragment.kt
@@ -1,14 +1,21 @@
 package com.kinshuk.notestakingapp.fragments
 
+import android.app.KeyguardManager
 import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.*
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.addCallback
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import androidx.appcompat.view.SupportActionModeWrapper
 import androidx.fragment.app.Fragment
 import androidx.appcompat.widget.SearchView
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.kinshuk.notestakingapp.MainActivity
@@ -17,6 +24,7 @@ import com.kinshuk.notestakingapp.adapter.NoteAdapter
 import com.kinshuk.notestakingapp.databinding.FragmentHomeBinding
 import com.kinshuk.notestakingapp.model.Note
 import com.kinshuk.notestakingapp.viewmodel.NoteViewModel
+import kotlin.math.log
 
 
 class HomeFragment : Fragment(R.layout.fragment_home)  , SearchView.OnQueryTextListener{
@@ -27,11 +35,14 @@ class HomeFragment : Fragment(R.layout.fragment_home)  , SearchView.OnQueryTextL
     private lateinit var notesViewModel: NoteViewModel
     private lateinit var notesAdapter: NoteAdapter
 
+    private val CODE_AUTHENTICATION_VERIFICATION = 241
+    private lateinit var  sharedPref: SharedPreferences
+
+    private lateinit var menu: Menu
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
-
-
     }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,7 +57,7 @@ class HomeFragment : Fragment(R.layout.fragment_home)  , SearchView.OnQueryTextL
         super.onViewCreated(view, savedInstanceState)
         notesViewModel = (activity as MainActivity).noteViewModel
 
-
+        sharedPref = requireActivity().getPreferences(Context.MODE_PRIVATE) ?: return
         setUpRecyclerVeiw()
         binding.addbtn.setOnClickListener{
             it.findNavController().navigate(R.id.action_homeFragment_to_newNoteFragment)
@@ -96,8 +107,18 @@ class HomeFragment : Fragment(R.layout.fragment_home)  , SearchView.OnQueryTextL
         val mMenuSearch = menu.findItem(R.id.menu_search).actionView as SearchView
         mMenuSearch.isSubmitButtonEnabled = false
         mMenuSearch.setOnQueryTextListener(this)
+        this.menu = menu
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val enabled =  sharedPref.getBoolean(getString(R.string.sharedPrefKey),false)
+        when(item.itemId){
+            R.id.toggle_lock->{
+                LockScreen()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
     override fun onQueryTextSubmit(p0: String?): Boolean {
 //        searchNote(p0!!)
         return false
@@ -133,6 +154,59 @@ class HomeFragment : Fragment(R.layout.fragment_home)  , SearchView.OnQueryTextL
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    private fun LockScreen() {
+        val km = requireActivity().getSystemService(AppCompatActivity.KEYGUARD_SERVICE) as KeyguardManager
+        if (km.isKeyguardSecure) {
+            val enabled = sharedPref.getBoolean(getString(R.string.sharedPrefKey),false)
+            var desc:String
+            if(enabled)
+                desc = "Use your password to disable App Lock"
+            else
+                desc = "Use your password to enable App Lock"
+            val i =
+                km.createConfirmDeviceCredentialIntent("Authentication required", desc)
+            startActivityForResult(i, CODE_AUTHENTICATION_VERIFICATION)
+        }
+        else {
+            Toast.makeText(
+                context,
+                "Need to setup Phone Lock Screen Authentication to use App Lock",
+                Toast.LENGTH_LONG
+            ).show()
+            sharedPref.edit().putBoolean(getString(R.string.sharedPrefKey),false).apply()
+        }
+    }
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        super.onPrepareOptionsMenu(menu)
+        val enabled = sharedPref.getBoolean(getString(R.string.sharedPrefKey),false)
+        val item = menu.findItem(R.id.toggle_lock)
+        item.title = if (enabled) "Disable App Lock" else "Enable App Lock"
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == AppCompatActivity.RESULT_OK && requestCode == CODE_AUTHENTICATION_VERIFICATION) {
+            val enabled =  sharedPref.getBoolean(getString(R.string.sharedPrefKey),false)
+            val editor = sharedPref.edit()
+            var item = menu.findItem(R.id.toggle_lock)
+            editor.putBoolean(getString(R.string.sharedPrefKey),!enabled).apply()
+
+            if(enabled) {
+                item.title = "Disable App Lock"
+                Toast.makeText(context, "App Lock Disabled", Toast.LENGTH_LONG).show()
+            }
+            else {
+                item.title = "Enable App Lock"
+                Toast.makeText(context, "App Lock Enabled", Toast.LENGTH_LONG).show()
+            }
+            activity?.invalidateOptionsMenu()
+            activity?.invalidateMenu()
+        }
+        else {
+            Toast.makeText(context, "Failure: Unable to verify user's identity", Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 
 }
